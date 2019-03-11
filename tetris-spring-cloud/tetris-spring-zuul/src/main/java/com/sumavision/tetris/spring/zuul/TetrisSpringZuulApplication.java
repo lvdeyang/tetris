437b004eac23f07549af3fe8fd5d6788d8939b27
463034590@qq.com <463034590@qq.com>
2019-03-11 13:07:38 +0800

    网关

--- tetris-spring-cloud/tetris-spring-zuul/src/main/java/com/sumavision/tetris/spring/zuul/TetrisSpringZuulApplication.java
+++ tetris-spring-cloud/tetris-spring-zuul/src/main/java/com/sumavision/tetris/spring/zuul/TetrisSpringZuulApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.sumavision.tetris.spring.zuul.auth.filter.AccessTokenFilter;
 
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TetrisSpringZuulApplication extends SpringBootServletInitializer{
 
@@ -20,5 +25,10 @@ public class TetrisSpringZuulApplication extends SpringBootServletInitializer{
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TetrisSpringZuulApplication.class);
     }
+	
+	@Bean
+	public AccessTokenFilter accessTokenFilter() {
+		return new AccessTokenFilter();
+	}
 	
 }
