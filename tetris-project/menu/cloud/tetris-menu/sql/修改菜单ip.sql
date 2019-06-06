271a11694df7b6c3965c37f24ab48566d9216d7c
463034590@qq.com <463034590@qq.com>
2019-06-06 17:41:53 +0800

    文档  初始化sql

---
+++ tetris-project/menu/cloud/tetris-menu/sql/修改菜单ip.sql
@@ -0,0 +1,6 @@
+update tetris_menu set link=replace(link,'192.165.56.85:8083','192.165.56.60:8083');
+update tetris_menu set link=replace(link,'192.165.56.85:8084','192.165.56.71:8084');
+update tetris_menu set link=replace(link,'192.165.56.85:8085','192.165.56.71:8085');
+update tetris_menu set link=replace(link,'192.165.56.85:8086','192.165.56.71:8086');
+update tetris_menu set link=replace(link,'192.165.56.85:8087','192.165.56.71:8087');
+update tetris_menu set link=replace(link,'192.165.56.85:8088','192.165.56.85:8088');
