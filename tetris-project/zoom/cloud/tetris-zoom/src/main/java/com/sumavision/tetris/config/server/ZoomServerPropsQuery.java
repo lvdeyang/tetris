86a08ec8fc05e4ad410fe9e630e910294cdbde49
463034590@qq.com <463034590@qq.com>
2020-02-28 15:31:47 +0800

    修改工程

--- tetris-project/zoom/cloud/tetris-zoom/src/main/java/com/sumavision/tetris/config/server/ZoomServerPropsQuery.java
+++ tetris-project/zoom/cloud/tetris-zoom/src/main/java/com/sumavision/tetris/config/server/ZoomServerPropsQuery.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WebrtcZoomServerPropsQuery {
+public class ZoomServerPropsQuery {
 
 	@Autowired
 	private ServerProps serverProps;
 	
 	/**
-	 * webrtc会议服务属性<br/>
+	 * 会议服务属性<br/>
 	 * <b>作者:</b>lvdeyang<br/>
 	 * <b>版本：</b>1.0<br/>
 	 * <b>日期：</b>2019年3月4日 下午3:11:36
