25c63039732387c9e4749e3a66b29424efb8ad59
wujiawen <sm@DESKTOP-GTO76RG>
2020-03-05 17:48:41 +0800

    资源

---
+++ tetris-project/resource/sdk/tetris-resource-sdk/src/main/java/com/suma/venus/resource/base/bo/EncoderBO.java
@@ -0,0 +1,63 @@
+package com.suma.venus.resource.base.bo;
+
+import com.sumavision.tetris.orm.exception.ErrorTypeException;
+
+public class EncoderBO {
+
+	private String encoderId;
+	
+	private String encoderName;
+	
+	private ENCODER_TYPE encoderType;
+	
+	public enum ENCODER_TYPE{
+		JV210("jv210"), 
+		ENCODER("encoder");
+		
+		private String name;
+		
+		public String getName() {
+			return name;
+		}
+
+		private ENCODER_TYPE(String name){
+			this.name = name;
+		}
+		
+		public static ENCODER_TYPE fromName(String name) throws Exception{
+			ENCODER_TYPE[] values = ENCODER_TYPE.values();
+			for(ENCODER_TYPE value: values){
+				if(value.getName().equals(name)){
+					return value;
+				}
+			}
+			
+			throw new ErrorTypeException("name", name);
+		}
+	}
+
+	public String getEncoderId() {
+		return encoderId;
+	}
+
+	public void setEncoderId(String encoderId) {
+		this.encoderId = encoderId;
+	}
+
+	public String getEncoderName() {
+		return encoderName;
+	}
+
+	public void setEncoderName(String encoderName) {
+		this.encoderName = encoderName;
+	}
+
+	public ENCODER_TYPE getEncoderType() {
+		return encoderType;
+	}
+
+	public void setEncoderType(ENCODER_TYPE encoderType) {
+		this.encoderType = encoderType;
+	}
+	
+}
