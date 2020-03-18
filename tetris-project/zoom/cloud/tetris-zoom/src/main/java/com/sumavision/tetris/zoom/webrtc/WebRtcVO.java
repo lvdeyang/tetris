c6cbbf9d1fcd125ca027f45a5c7ad13b809c64a2
wujiawen <sm@DESKTOP-GTO76RG>
2020-03-18 10:28:14 +0800

    commit

---
+++ tetris-project/zoom/cloud/tetris-zoom/src/main/java/com/sumavision/tetris/zoom/webrtc/WebRtcVO.java
@@ -0,0 +1,38 @@
+package com.sumavision.tetris.zoom.webrtc;
+
+public class WebRtcVO {
+
+	private String ip;
+	
+	private String httpPort;
+	
+	private String websocketPort;
+
+	public String getIp() {
+		return ip;
+	}
+
+	public WebRtcVO setIp(String ip) {
+		this.ip = ip;
+		return this;
+	}
+
+	public String getHttpPort() {
+		return httpPort;
+	}
+
+	public WebRtcVO setHttpPort(String httpPort) {
+		this.httpPort = httpPort;
+		return this;
+	}
+
+	public String getWebsocketPort() {
+		return websocketPort;
+	}
+
+	public WebRtcVO setWebsocketPort(String websocketPort) {
+		this.websocketPort = websocketPort;
+		return this;
+	}
+	
+}
