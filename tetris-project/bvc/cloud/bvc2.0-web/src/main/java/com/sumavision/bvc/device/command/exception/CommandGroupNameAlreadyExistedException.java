e9adb8abd6e6b8f1bc57d91f4468cebd034f2cd3
PolarNightDH <polarnight@foxmail.com>
2020-03-25 18:04:21 +0800

    合并以前改的代码

--- tetris-project/bvc/cloud/bvc2.0-web/src/main/java/com/sumavision/bvc/device/command/exception/CommandGroupNameAlreadyExistedException.java
+++ tetris-project/bvc/cloud/bvc2.0-web/src/main/java/com/sumavision/bvc/device/command/exception/CommandGroupNameAlreadyExistedException.java
@@ -8,11 +8,30 @@ public class CommandGroupNameAlreadyExistedException extends BaseException {
 
 	public static final long serialVersionUID = 1L;
 	
+	//协议码
+	private String recommendedName;
+	
 	public CommandGroupNameAlreadyExistedException(String groupName){
 		super(StatusCode.FORBIDDEN, new StringBufferWrapper().append("会议：")
 															 .append(groupName)
-															 .append("已经存在，请修改名称或取消操作！")
+															 .append("已经存在，请修改名称")
 															 .toString());
+	}
+	
+	public CommandGroupNameAlreadyExistedException(String groupName, String recommendedName){
+		super(StatusCode.FORBIDDEN, new StringBufferWrapper().append("会议：")
+															 .append(groupName)
+															 .append("已经存在，请修改名称")
+															 .toString());
+		this.setRecommendedName(recommendedName);
+	}
+
+	public String getRecommendedName() {
+		return recommendedName;
+	}
+
+	public void setRecommendedName(String recommendedName) {
+		this.recommendedName = recommendedName;
 	}
 	
 }
