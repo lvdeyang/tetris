67adf5d3e3823680edd09c32447eb6ddc0ae1283
chenmo <chenmoalex@outlook.com>
2020-03-26 08:24:06 +0800

    chenmo: record提交

--- tetris-project/record/cloud/tetris-record/src/main/java/com/sumavision/tetris/record/TetrisRecordApplication.java
+++ tetris-project/record/cloud/tetris-record/src/main/java/com/sumavision/tetris/record/TetrisRecordApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = { "com.sumavision.tetris" })
+@ServletComponentScan(basePackages = { "com.sumavision.tetris" })
 @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 @SpringBootApplication
 @EnableCaching
