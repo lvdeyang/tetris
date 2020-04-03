dc3e653c31542a8f997c56685d1d7286567133fe
[surpass-yan] <[3060841780@qq.com]>
2020-04-03 10:40:44 +0800

    冲突

--- tetris-project/resource/feign/tetris-resource-feign/src/main/java/com/sumavision/tetris/resouce/feign/resource/ResourceService.java
+++ tetris-project/resource/feign/tetris-resource-feign/src/main/java/com/sumavision/tetris/resouce/feign/resource/ResourceService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.sumavision.tetris.mvc.ext.response.parser.JsonBodyResponseParser;
 
 @Component
