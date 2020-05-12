91aaa59ba00af578595465e5678b859c2d3889c3
chenmo <chenmoalex@outlook.com>
2020-05-12 13:03:37 +0800

    chenmo: 添加设备feign接口

--- tetris-project/resource/feign/tetris-resource-feign/src/main/java/com/sumavision/tetris/resouce/feign/bundle/BundleFeign.java
+++ tetris-project/resource/feign/tetris-resource-feign/src/main/java/com/sumavision/tetris/resouce/feign/bundle/BundleFeign.java
@@ -11,38 +11,41 @@ import com.sumavision.tetris.config.feign.FeignConfiguration;
 @FeignClient(name = "tetris-resource", configuration = FeignConfiguration.class)
 public interface BundleFeign {
 
+	@RequestMapping(value = "/feign/bundle/addTranscodeDevice", method = RequestMethod.POST)
+	public String addTranscodeDevice(@RequestParam(value = "name") String name, @RequestParam(value = "ip") String ip,
+			@RequestParam(value = "port") Integer port) throws Exception;
+
 	/**
 	 * 查询转码设备<br/>
 	 */
 	@RequestMapping(value = "/feign/bundle/queryTranscodeDevice", method = RequestMethod.POST)
 	public String queryTranscodeDevice() throws Exception;
-	
+
 	/**
 	 * 查询转码设备<br/>
 	 */
 	@RequestMapping(value = "/feign/bundle/queryDeviceByBundleId", method = RequestMethod.POST)
 	public String queryDeviceByBundleId(@RequestParam(value = "bundle_id") String bundle_id) throws Exception;
 
-
 	/**
 	 * 查询设备的授权<br/>
 	 */
 	@RequestMapping(value = "/feign/bundle/queryAuth", method = RequestMethod.POST)
-	public JSONObject queryAuth(@RequestParam(value = "bundle_id")String bundle_id) throws Exception;
-	
+	public JSONObject queryAuth(@RequestParam(value = "bundle_id") String bundle_id) throws Exception;
+
 	/**
 	 * 查询经纬度范围内的设备<br/>
 	 * <b>作者:</b>wjw<br/>
 	 * <b>版本：</b>1.0<br/>
 	 * <b>日期：</b>2020年3月6日 下午5:13:02
+	 * 
 	 * @param Long longitude 经度
 	 * @param Long latitude 纬度
 	 * @param Long raidus 半径范围
 	 */
 	@RequestMapping(value = "/feign/bundle/query/visible/bundle", method = RequestMethod.POST)
-	public JSONObject queryVisibleBundle(
-			@RequestParam(value = "longitude") String longitude,
-			@RequestParam(value = "latitude") String latitude,
-			@RequestParam(value = "raidus") Long raidus) throws Exception;
+	public JSONObject queryVisibleBundle(@RequestParam(value = "longitude") String longitude,
+			@RequestParam(value = "latitude") String latitude, @RequestParam(value = "raidus") Long raidus)
+			throws Exception;
 
 }
