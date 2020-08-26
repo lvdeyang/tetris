585c1471c09e5e7869df80b89a63c5fbe0fea219
ldyReviewer <787281439@qq,com>
2020-08-26 10:55:17 +0800

    又搞了两个类

---
+++ tetris-project/omms/cloud/tetris-omms/src/main/java/com/sumavision/tetris/omms/software/service/installation/InstallationPackageVO.java
@@ -0,0 +1,12 @@
+package com.sumavision.tetris.omms.software.service.installation;
+
+import com.sumavision.tetris.mvc.converter.AbstractBaseVO;
+
+public class InstallationPackageVO extends AbstractBaseVO<InstallationPackageVO, InstallationPackagePO>{
+
+	@Override
+	public InstallationPackageVO set(InstallationPackagePO entity) throws Exception {
+		return null;
+	}
+
+}
---
+++ tetris-project/omms/cloud/tetris-omms/src/main/java/com/sumavision/tetris/omms/software/service/installation/history/InstallationPackageHistoryVO.java
@@ -0,0 +1,12 @@
+package com.sumavision.tetris.omms.software.service.installation.history;
+
+import com.sumavision.tetris.mvc.converter.AbstractBaseVO;
+
+public class InstallationPackageHistoryVO extends AbstractBaseVO<InstallationPackageHistoryVO, InstallationPackageHistoryPO>{
+
+	@Override
+	public InstallationPackageHistoryVO set(InstallationPackageHistoryPO entity) throws Exception {
+		return null;
+	}
+
+}
