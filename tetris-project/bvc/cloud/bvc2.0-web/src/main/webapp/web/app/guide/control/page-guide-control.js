d951837ca257de90e1ec2b0e03908760a246bf99
fish <Administrator@windows10.microdone.cn>
2020-09-07 13:59:08 +0800

    导播页面

--- tetris-project/bvc/cloud/bvc2.0-web/src/main/webapp/web/app/guide/control/page-guide-control.js
+++ tetris-project/bvc/cloud/bvc2.0-web/src/main/webapp/web/app/guide/control/page-guide-control.js
@@ -28,6 +28,7 @@ define([
                 	menurouter: false,
                     shortCutsRoutes:commons.data,
                     active:"/page-group-list",
+					curPgm:1,
                     dialog: {
                     	setSource:{
                     		visible: false,
@@ -36,7 +37,9 @@ define([
                             type:'',
                             device:{},
                             url:'',
-                            typeOptions:['Stream','Device']
+							index:0,
+							previewOut:'',
+                            typeOptions:['直播流','5G背包']
                     	},
                     	setOut:{
                     		visible: false,
@@ -44,7 +47,13 @@ define([
                             type:'',
                             url:'',
                             typeOptions:['UDP','HLS']
-                    	}
+                    	},
+						selDevice:{
+							visible: false,
+                            loading: false,
+							deviceData:[],
+							
+						}
                     }
                 }
             },
@@ -55,11 +64,84 @@ define([
             	handleSetSource:function(index){
             		var self=this;
             		self.dialog.setSource.visible=true;
+					self.dialog.setSource.index=index;
+					self.dialog.setSource.type="";
+					self.dialog.setSource.url="";
+					self.dialog.setSource.previewOut="";
+					self.dialog.setSource.device={};
             	},
+				handleSetSourceCommit:function(){
+					var self = this;
+					self.dialog.setSource.visible=false;
+					var questData = {
+						type: self.dialog.setSource.type,
+						url:self.dialog.setSource.url,
+						deviceId:self.dialog.setSource.device.id,
+						name:self.dialog.setSource.device.name,
+						index:self.dialog.setSource.index,
+						previewOut:self.dialog.setSource.previewOut
+					};
+					ajax.post('/guide/control/source/set', questData, function (data, status) {
+						
+					}, null, ajax.NO_ERROR_CATCH_CODE);
+				},
+				handleSetSourceClose:function(){
+					var self = this;
+					self.dialog.setSource.visible=false;
+				},
             	handleSetOut:function(){
             		var self=this;
             		self.dialog.setOut.visible=true;
-            	}
+					self.dialog.setOut.type="";
+					self.dialog.setOut.url="";
+            	},
+				handleSetOutCommit:function(){
+					var self = this;
+					self.dialog.setOut.visible=false;
+					var questData = {
+						type: self.dialog.setOut.type,
+						url:self.dialog.setOut.url
+					};
+					ajax.post('/guide/control/output/set', questData, function (data, status) {
+						
+					}, null, ajax.NO_ERROR_CATCH_CODE);
+
+				},
+				handleSetOutClose:function(){
+					var self=this;
+            		self.dialog.setOut.visible=false;
+				},
+				handleDeviceSelect:function(){
+					var self=this;
+					self.dialog.setSource.visible=false;
+					self.dialog.selDevice.visible=true;
+				},
+				startGuide:function(){
+					
+				},
+				stopGuide:function(){
+					
+				},
+				handleSelPgm:function(index){
+					var self=this;
+					self.curPgm=index;
+				},
+				getBackcolor:function(index){
+					var self=this;
+					if(self.curPgm==index){
+						return "background:#cc0033"
+					}else{
+						return "background:#CCC"
+					}
+				},switchSource(){
+					var self = this;
+					var questData = {
+						index: self.curPgm
+					};
+					ajax.post('/guide/control/source/switch', questData, function (data, status) {
+						
+					}, null, ajax.NO_ERROR_CATCH_CODE);
+				}
                
             }
         });
