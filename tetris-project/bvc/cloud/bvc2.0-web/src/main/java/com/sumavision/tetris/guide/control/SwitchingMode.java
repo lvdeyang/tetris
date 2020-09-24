9dacec9f311a29f8ab2305f510c877abdaf2dbd9
jiajun <jiajun@dvt.dvt.com>
2020-09-24 08:52:56 +0800

    添加字段

---
+++ tetris-project/bvc/cloud/bvc2.0-web/src/main/java/com/sumavision/tetris/guide/control/SwitchingMode.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.sumavision.tetris.guide.control;
+
+import com.sumavision.tetris.orm.exception.ErrorTypeException;
+
+/**
+ * 类型概述<br/>
+ * <p>详细描述</p>
+ * <b>作者:</b>Administrator<br/>
+ * <b>版本：</b>1.0<br/>
+ * <b>日期：</b>2020年9月23日 下午3:52:43
+ */
+public enum SwitchingMode {
+	
+	TRANSCODE("转码"),
+	FRAME("按帧切换"),
+	DIRECTOR("直接切换");
+	
+	private String name;
+	
+	private SwitchingMode(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public static SwitchingMode fromName(String name) throws Exception{
+		SwitchingMode[] value = SwitchingMode.values();
+		for (SwitchingMode switchingMode : value) {
+			if(switchingMode.getName().equals(name)){
+				return switchingMode;
+			}
+		}
+		throw new ErrorTypeException("name", name);
+	}
+}
