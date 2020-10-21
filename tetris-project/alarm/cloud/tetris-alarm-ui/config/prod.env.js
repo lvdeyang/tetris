368c32a6d557b6ea4747565fdfb73fa04ae35792
王凯 <wangkai@sumavision.cn>
2020-10-21 17:05:32 +0800

    资源系统关联bvc中站点接口

--- tetris-project/alarm/cloud/tetris-alarm-ui/config/prod.env.js
+++ tetris-project/alarm/cloud/tetris-alarm-ui/config/prod.env.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict'
 
 // 重要
@@ -11,18 +11,17 @@ module.exports = {
   // USER_ROOT: useLocalRouteConst? '"https://__requestIP__:8543"' :  '"https://__applicationIP__:8543"'
   ALARM_ROOT:  useLocalRouteConst? '"https://__requestIP__:9143"' : '"https://__applicationIP__:9143"',
   WEBSOCKET_URL : useLocalRouteConst? '"wss://__requestIP__:9143/"': '"wss://__applicationIP__:9143/"'
-=======
-'use strict'
-
-// 重要
-const useLocalRouteConst = true
-
-module.exports = {
-  NODE_ENV: '"production"',
-  USELOCALROUTE : useLocalRouteConst,
-  // RESOURCE_ROOT: useLocalRouteConst? '"https://__requestIP__:8743"': '"https://__applicationIP__:8743"',
-  // USER_ROOT: useLocalRouteConst? '"https://__requestIP__:8543"' :  '"https://__applicationIP__:8543"'
-  ALARM_ROOT:  useLocalRouteConst? '"http://__requestIP__:8097"' : '"http://10.10.40.27:8092"',
-  WEBSOCKET_URL : useLocalRouteConst? '"ws://__requestIP__:8097/"': '"ws://10.10.40.27:8092/"'
->>>>>>> remotes/origin/master
 }
+// 'use strict'
+
+// // 重要
+// const useLocalRouteConst = true
+
+// module.exports = {
+//   NODE_ENV: '"production"',
+//   USELOCALROUTE : useLocalRouteConst,
+//   // RESOURCE_ROOT: useLocalRouteConst? '"https://__requestIP__:8743"': '"https://__applicationIP__:8743"',
+//   // USER_ROOT: useLocalRouteConst? '"https://__requestIP__:8543"' :  '"https://__applicationIP__:8543"'
+//   ALARM_ROOT:  useLocalRouteConst? '"http://__requestIP__:8097"' : '"http://10.10.40.27:8092"',
+//   WEBSOCKET_URL : useLocalRouteConst? '"ws://__requestIP__:8097/"': '"ws://10.10.40.27:8092/"'
+// }
