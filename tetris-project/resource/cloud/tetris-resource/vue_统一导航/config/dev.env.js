d07596fc074a8453ef1e2c43959a2478cdfe6c20
王凯 <wangkai@sumavision.cn>
2020-10-22 13:10:00 +0800

    资源列表分站点功能

--- tetris-project/resource/cloud/tetris-resource/vue_统一导航/config/dev.env.js
+++ tetris-project/resource/cloud/tetris-resource/vue_统一导航/config/dev.env.js
@@ -8,6 +8,10 @@ const useLocalRouteConst = false
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   USELOCALROUTE : useLocalRouteConst,
-  RESOURCE_ROOT: useLocalRouteConst ? '"http://__requestIP__:8093"': '"http://192.165.56.111:8093"',
-  USER_ROOT: useLocalRouteConst? '"http://__requestIP__:8093"' : '"http://192.165.56.111:8093"'
+  // RESOURCE_ROOT: useLocalRouteConst ? '"http://__requestIP__:8093"': '"http://192.165.56.111:8093"',
+  // USER_ROOT: useLocalRouteConst? '"http://__requestIP__:8093"' : '"http://192.165.56.111:8093"'
+  
+  RESOURCE_ROOT: useLocalRouteConst ? '"http://__requestIP__:8093"': '"https://192.165.56.131:8213/"',
+  USER_ROOT: useLocalRouteConst? '"http://__requestIP__:8093"' : '"https://192.165.56.131:8213/"'
+  
 })
