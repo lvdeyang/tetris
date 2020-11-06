4013ee2b0e6c59b57a502368c31327d19665591b
ldyReviewer <787281439@qq,com>
2020-11-06 16:22:35 +0800

    大会页面联系人管理

--- tetris-project/bvc/cloud/bvc2.0-web/src/main/java/com/sumavision/bvc/control/device/group/contacts/DeviceGroupContactsDAO.java
+++ tetris-project/bvc/cloud/bvc2.0-web/src/main/java/com/sumavision/bvc/control/device/group/contacts/DeviceGroupContactsDAO.java
@@ -10,7 +10,16 @@ import com.sumavision.tetris.orm.dao.MetBaseDAO;
 @RepositoryDefinition(domainClass = DeviceGroupContactsPO.class, idClass = Long.class)
 public interface DeviceGroupContactsDAO extends MetBaseDAO<DeviceGroupContactsPO>{
 
-	public void deleteByUserIdAndBundleIdIn(Long userId, Collection<String> bundleIdList);
+	/**
+	 * 根据用户id和bundleId查询用户联系人<br/>
+	 * <b>作者:</b>lvdeyang<br/>
+	 * <b>版本：</b>1.0<br/>
+	 * <b>日期：</b>2020年11月6日 下午2:45:32
+	 * @param Long userId 用户id
+	 * @param Collection<String> bundleIdList 设备id列表
+	 * @return List<DeviceGroupContactsPO> 联系人列表
+	 */
+	public List<DeviceGroupContactsPO> findByUserIdAndBundleIdIn(Long userId, Collection<String> bundleIdList);
 	
-	public List<String> findBundleIdByUserId(Long userId);
+	public List<DeviceGroupContactsPO> findByUserId(Long userId);
 }
