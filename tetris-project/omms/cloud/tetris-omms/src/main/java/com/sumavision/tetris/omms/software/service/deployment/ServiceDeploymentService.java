c0a594ec10c5b62e6aaf19ad1e7701541c576025
zhouaining <Administrator@PC-20200902QLYH>
2020-11-30 18:11:35 +0800

    1111

--- tetris-project/omms/cloud/tetris-omms/src/main/java/com/sumavision/tetris/omms/software/service/deployment/ServiceDeploymentService.java
+++ tetris-project/omms/cloud/tetris-omms/src/main/java/com/sumavision/tetris/omms/software/service/deployment/ServiceDeploymentService.java
@@ -5,12 +5,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
-import java.io.InputStream;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
