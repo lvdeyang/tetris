750958ad8c3c63b8a6bb3736d17d2fee5bfff0c1
闫智宣 <yanzhixuan@dvt.dvt.com>
2020-12-09 09:34:19 +0800

    撤销 "合并分支 'yanzhixuan' 到 'master'"
    这将还原合并请求 !375

--- tetris-project/capacity/cloud/tetris-capacity/src/main/java/com/sumavision/tetris/business/transcode/vo/AnalysisStreamVO.java
+++ tetris-project/capacity/cloud/tetris-capacity/src/main/java/com/sumavision/tetris/business/transcode/vo/AnalysisStreamVO.java
@@ -2,10 +2,6 @@ package com.sumavision.tetris.business.transcode.vo;/**
  * Created by Poemafar on 2020/8/31 16:21
  */
 
-import com.sumavision.tetris.capacity.bo.input.InputBO;
-
-import java.util.List;
-
 /**
  * @ClassName: AnalysisStreamVO
  * @Description TODO
@@ -15,15 +11,11 @@ import java.util.List;
  */
 public class AnalysisStreamVO {
 
-    private String deviceIp;
-
     private String msg_id;
 
     private String type;
 
     private String inputId;
-
-    private List<InputBO> input_array;
 
     public String getMsg_id() {
         return msg_id;
@@ -47,21 +39,5 @@ public class AnalysisStreamVO {
 
     public void setInputId(String inputId) {
         this.inputId = inputId;
-    }
-
-    public List<InputBO> getInput_array() {
-        return input_array;
-    }
-
-    public void setInput_array(List<InputBO> input_array) {
-        this.input_array = input_array;
-    }
-
-    public String getDeviceIp() {
-        return deviceIp;
-    }
-
-    public void setDeviceIp(String deviceIp) {
-        this.deviceIp = deviceIp;
     }
 }
