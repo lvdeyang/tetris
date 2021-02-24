739a76ad5c8b801f2236b8942765f5c2d99f2d01
463034590@qq.com <463034590@qq.com>
2021-02-24 08:56:36 +0800

    用户角色菜单权限优化--系统运维部分

---
+++ tetris-project/user/cloud/tetris-user/src/main/webapp/web/app/company-system-role/page-company-system-role.i18n.js
@@ -0,0 +1,39 @@
+/**
+ * Created by lvdeyang on 2020/4/15 0015.
+ */
+define(function(){
+
+    var zhCN = {
+        title:'系统角色',
+        homePage:'首页',
+        currentPage:'系统角色',
+        handleCreateSystemRole:'添加角色',
+        tableColumnName:'角色',
+        tableColumnOperation:'操作',
+        tipHandleRowEdit:'修改角色',
+        tipHandleBindMenu:'功能授权',
+        tipHandleRowDelete:'删除角色',
+        dialogCreateSystemRoleGroupTitle:'新建系统角色组',
+        placeholderCreateSystemRoleGroupName:'起个名吧！',
+        handleCreateSystemRoleGroupClose:'取消',
+        handleCreateSystemRoleGroupSubmit:'确定',
+        dialogEditSystemRoleGroupTitle:'编辑系统角色组',
+        placeholderEditSystemRoleGroupName:'改个名吧！',
+        handleEditSystemRoleGroupCancel:'取消',
+        handleEditSystemRoleGroupSubmit:'确定',
+        dialogCreateSystemRoleTitle:'新建系统角色',
+        placeholderCreateSystemRoleName:'起个名吧！',
+        handleCreateSystemRoleCancel:'取消',
+        handleCreateSystemRoleSubmit:'确定',
+        dialogEditSystemRoleTitle:'编辑系统角色',
+        placeholderEditSystemRoleName:'改个名吧！',
+        handleEditSystemRoleCancel:'取消',
+        handleEditSystemRoleSubmit:'确定',
+        dialogBindMenuTitle:'功能授权'
+    };
+
+    return{
+        'default':zhCN,
+        'zh-CN':zhCN
+    }
+});
